c5e5415c7411329bcaf6a98e24c0d94e64183945
pinehill10981 <danvitelli15@gmail.com>
2021-05-30 18:11:05 -0500

    updated event interactions

--- src/messaging/EventEmbeds.ts
+++ src/messaging/EventEmbeds.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed } from "discord.js";
+import { MessageEmbed, User } from "discord.js";
 import ScheduledEvent from "../data/entities/Event";
 import * as colors from "../settings/MessageEmbedColors";
 
@@ -7,5 +7,27 @@ export const EventDetails = (event: ScheduledEvent): MessageEmbed => {
   embed.setTitle(event.Title);
   embed.setColor(colors.PRIMARRY);
   embed.setDescription(event.Description);
+  embed.addField("On", event.Date?.toFormat("DDDD") || "TBD");
+  return embed;
+};
+
+export const EventDetailsUpdate = (
+  event: ScheduledEvent,
+  field: string,
+  updatedBy: User,
+  newValue: string
+): MessageEmbed => {
+  const embed = new MessageEmbed();
+  embed.setColor(colors.INFO);
+  embed.setAuthor(`${updatedBy.username}: Updated ${event.Title}`, updatedBy.avatarURL() || undefined);
+  embed.addField(field, newValue);
+  return embed;
+};
+
+export const Directions = (event: ScheduledEvent): MessageEmbed => {
+  const embed = new MessageEmbed();
+  const address = "";
+  // @ts-ignore : TS doesnt have replaceAll?
+  const mapsURL = `https://www.google.com/maps/dir//${address.replaceAll(" ", "+")}`;
   return embed;
 };
--- src/tasks/commandSets/events/EventInteraction.ts
+++ src/tasks/commandSets/events/EventInteraction.ts
@@ -3,7 +3,7 @@ import ScheduledEvent from "../../../data/entities/Event";
 import RSVP, { RSVPConstructor } from "../../../data/entities/RSVP";
 import Store from "../../../data/Store";
 import { GetDiscordUser } from "../../../Environment";
-import { EventDetails } from "../../../messaging/EventEmbeds";
+import { EventDetails, EventDetailsUpdate } from "../../../messaging/EventEmbeds";
 import { GuestList } from "../../../messaging/RSVPEmbeds";
 
 export default async (
@@ -18,7 +18,43 @@ export default async (
       return EventDetails(event);
     }
     case words.includes("~rename"): {
-      break;
+      const eventIndex = words.indexOf("~rename");
+      let eventEndIndex = words.slice(eventIndex + 1).findIndex((i) => i.includes("~"));
+      if (eventEndIndex < 0) {
+        eventEndIndex = words.length;
+      } else {
+        eventEndIndex += eventIndex + 1;
+      }
+      const title = "".concat(...words.slice(eventIndex + 1, eventEndIndex).map((i) => i + " ")).trim();
+      event.setTitle(title);
+      message.delete({ reason: "Replacing with renamed event details." });
+      return EventDetailsUpdate(event, "Title", message.author, event.Title);
+    }
+    case words.includes("~on"): {
+      const dateIndex = words.indexOf("~on");
+      if (dateIndex >= 0 && dateIndex + 1 < words.length) {
+        if (words[dateIndex + 1] !== null) {
+          event.setDate(words[dateIndex + 1]);
+        }
+      }
+      message.delete({ reason: "Replacing with renamed event details." });
+      return EventDetailsUpdate(event, "Date", message.author, event.Date?.toFormat("DDDD") || "TBD");
+    }
+    case words.includes("~for"): {
+      const descriptionIndex = words.indexOf("~for");
+      let description = "";
+      if (descriptionIndex >= 0 && descriptionIndex + 1 < words.length) {
+        let descriptionEndIndex = words.slice(descriptionIndex + 1).findIndex((i) => i.includes("~"));
+        if (descriptionEndIndex < 0) {
+          descriptionEndIndex = words.length;
+        } else {
+          descriptionEndIndex += descriptionIndex + 1;
+        }
+        description = "".concat(...words.slice(descriptionIndex + 1, descriptionEndIndex).map((i) => i + " ")).trim();
+        event.setDescription(description);
+      }
+      message.delete({ reason: "Replacing with renamed event details." });
+      return EventDetailsUpdate(event, "Description", message.author, description);
     }
     case words.includes("~guests"): {
       return GuestList(
